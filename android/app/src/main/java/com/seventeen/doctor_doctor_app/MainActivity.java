ca04ad49e44f8fcb04ad6d7b8aa8163be9f689ce
uaesmartlink <uaesmartlink@gmail.com>
2023-02-14 16:34:35 +0400

    something

--- android/app/src/main/java/com/seventeen/doctor_doctor_app/MainActivity.java
+++ android/app/src/main/java/com/seventeen/doctor_doctor_app/MainActivity.java
@@ -1,6 +1,13 @@
 package com.seventeen.doctor_doctor_app;
 
+import android.content.Context;
+import androidx.multidex.MultiDex;
 import io.flutter.embedding.android.FlutterActivity;
-
+import io.flutter.embedding.engine.FlutterEngine;
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
